f480b50230307a855503838b1a63085771a529ee
masud <abdullahalmasud01737906772@gmail.com>
2022-05-01 00:39:34 +0600

    connected computer warehouse

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
+//const { config } = require('dotenv');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,10 +14,20 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwpgf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Computer Warehouse DB Connected')
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 
 app.get('/', (req, res) =>{
-    res.send('Running react server');
+    res.send('Running warehouse server');
 });
 
 app.listen(port, () =>{
