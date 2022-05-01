2d3e3d7169df1067d99c09a7c883f21f179b0153
masud <abdullahalmasud01737906772@gmail.com>
2022-05-01 06:13:59 +0600

    add item

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 //const { config } = require('dotenv');
@@ -16,20 +16,48 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwpgf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Computer Warehouse DB Connected')
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoriesCollection = client.db('computerWarehouse').collection('inventories');
+
+        app.get('/inventories', async (req, res) => {
+            const query = {};
+            const cursor = inventoriesCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+
+        app.get('/inventories/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const inventories = await inventoriesCollection.findOne(query);
+            res.send(inventories);
+        });
+
+        // post 
+        app.post('/inventories', async(req, res) =>{
+            const newInventories =req.body;
+            const result = await inventoriesCollection.insertOne(newInventories);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Running warehouse server');
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Listening to port', port);
 })
